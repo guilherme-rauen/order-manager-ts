a21b533f724510cf976f00e158fdcfb97e3f5997
Guilherme Rauen <g.rauen@cognigy.com>
2023-09-22 10:15:21 +0200

    add logger

---
+++ src/logger.module.ts
@@ -0,0 +1,41 @@
+import pino from 'pino';
+
+interface LoggerMetadata {
+  module: string;
+  [key: string]: unknown;
+}
+
+export class Logger {
+  private logger: pino.Logger;
+
+  constructor(private readonly servicePrefix: string) {
+    this.logger = pino({
+      transport: {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          ignore: 'pid,hostname',
+          singleLine: true,
+        },
+      },
+      name: this.servicePrefix,
+      level: process.env.LOG_LEVEL || 'info',
+    });
+  }
+
+  public debug(message: string, metadata: LoggerMetadata) {
+    this.logger.debug(metadata, message);
+  }
+
+  public info(message: string, metadata: LoggerMetadata) {
+    this.logger.info(metadata, message);
+  }
+
+  public warn(message: string, metadata: LoggerMetadata) {
+    this.logger.warn(metadata, message);
+  }
+
+  public error(message: string, metadata: LoggerMetadata) {
+    this.logger.error(metadata, message);
+  }
+}
