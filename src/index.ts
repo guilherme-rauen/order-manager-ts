5eefbad3735c93a10ab7851715eabadaa623d79a
Guilherme Rauen <g.rauen@cognigy.com>
2023-09-22 10:41:25 +0200

    add main function

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+
+import { AppModule } from './app.module';
+import { Logger } from './logger.module';
+
+dotenv.config();
+
+try {
+  const logger = new Logger(process.env.APP_NAME ?? '');
+  const app = new AppModule(logger);
+  const main = async (): Promise<void> => {
+    app.start();
+  };
+
+  main().catch(error => {
+    logger.error(error.message, { error, module: 'index' });
+    process.exit(1);
+  });
+} catch (error) {
+  console.error(`Failed to initialize - ${error}`);
+  process.exit(1);
+}
