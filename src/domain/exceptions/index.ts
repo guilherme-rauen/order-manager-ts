4607209dbf054c453b39e73057869e475795ca46
Guilherme Rauen <g.rauen@cognigy.com>
2023-10-04 15:17:54 +0200

    add invalid order status exception

--- src/domain/exceptions/index.ts
+++ src/domain/exceptions/index.ts
@@ -1,4 +1,5 @@
 export { ControllerValidationException } from './controller-validation.exception';
 export { InstanceNotFoundException } from './instance-not-found.exception';
+export { InvalidOrderStatusException } from './invalid-order-status.exception';
 export { MissingEnvVarException } from './missing-env-var.exception';
 export { ObjectNotFoundException } from './object-not-found.exception';
---
+++ src/domain/exceptions/invalid-order-status.exception.ts
@@ -0,0 +1,3 @@
+import ExtendableError from 'ts-error';
+
+export class InvalidOrderStatusException extends ExtendableError {}
