d1431fd534b0ed2b742feaa34fedefdbca52d24f
Guilherme Rauen <g.rauen@cognigy.com>
2023-10-05 09:01:18 +0200

    add handler for uncaught exceptions and unhandled rejection

--- src/index.ts
+++ src/index.ts
@@ -5,18 +5,21 @@ import { Logger } from './logger.module';
 
 dotenv.config();
 
-try {
-  const logger = new Logger(process.env.APP_NAME ?? '');
-  const app = new AppModule(logger);
-  const main = async (): Promise<void> => {
-    await app.start();
-  };
+const logger = new Logger(process.env.APP_NAME ?? '');
+const app = new AppModule(logger);
+const main = async (): Promise<void> => {
+  await app.start();
+};
 
-  main().catch(error => {
-    logger.error(error.message, { error, module: 'index' });
-    process.exit(1);
-  });
-} catch (error) {
-  console.error(`Failed to initialize - ${error}`);
+main().catch(error => {
+  logger.error(error.message, { module: 'index', error });
   process.exit(1);
-}
+});
+
+process.on('uncaughtException', error => {
+  logger.error(`Uncaught exception error caught: ${error.message}`, { module: 'index', error });
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  logger.error(`Unhandled Rejection at: ${promise}, reason: ${reason}`, { module: 'index' });
+});
