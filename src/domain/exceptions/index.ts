cf0fb26120db37c9f3512d0b80b9f53a4f56e17f
Guilherme Rauen <g.rauen@cognigy.com>
2023-10-23 11:07:33 +0200

    add amount mismatch custom exception

--- src/domain/exceptions/index.ts
+++ src/domain/exceptions/index.ts
@@ -1,3 +1,4 @@
+export { AmountMismatchException } from './amount-mismatch.exception';
 export { ControllerValidationException } from './controller-validation.exception';
 export { InstanceNotFoundException } from './instance-not-found.exception';
 export { InvalidOrderStatusException } from './invalid-order-status.exception';
